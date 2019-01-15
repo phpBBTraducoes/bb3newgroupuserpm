014079f430d869660a107af8b3cbf90780ecaaaf
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-01-15 21:46:08 -0200

    ADD

---
+++ language/pt_br/acp_bb3newgroupuserpm.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+*
+* @package BB3NewGroupUserPM
+* @copyright (c) 2016 PPK
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* Brazilian Portuguese translation by eunaumtenhoid [2019][ver 1.0.0] (https://github.com/phpBBTraducoes)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'GROUPUSERPM_SETTINGS' => 'Mensagem privada ao entrar no grupo',
+	'BB3NEWGROUPUSERPM_USERID' => 'ID do usuário',
+	'BB3NEWGROUPUSERPM_USERID_EXPLAIN' => 'ID do usuário de quais mensagens privadas serão enviadas (0 - em nome do membro do grupo, esta opção será comum a todos os grupos)',
+	'BB3NEWGROUPUSERPM_SUBJECT' => 'Cabeçalho da mensagem',
+	'BB3NEWGROUPUSERPM_SUBJECT_EXPLAIN' => 'Pode estar na forma de texto ou um nome de variável (não use código HTML), a variável deve ser definida no arquivo /ext/ppk/bb3newgroupuserpm/language/seu idioma/acp_bb3newgroupuserpm.php',
+	'BB3NEWGROUPUSERPM_MESSAGE' => 'Texto da mensagem',
+	'BB3NEWGROUPUSERPM_MESSAGE_EXPLAIN' => 'Para enviar aos usuários uma mensagem privada ao entrar neste grupo, pode ser na forma de texto ou um nome de variável, a variável deve ser definida no arquivo /ext/ppk/bb3newgroupuserpm/language/seu idioma/acp_bb3newgroupuserpm.php, você pode usar o código HTML, bem como o seguinte variáveis:<br />{USER_ID} - ID do usuário <br />{USERNAME} - nome do usuário<br />{GROUP_ID} - ID do grupo<br />{GROUP_NAME} - nome do grupo<br />para enviar uma mensagem ser preenchido neste campo e no campo abaixo',
+
+	'GROUP_MESSAGE0_SUBJECT' => 'Exemplo de cabeçalho de mensagem',
+	'GROUP_MESSAGE0_MESSAGE' => 'Texto de exemplo',
+));
